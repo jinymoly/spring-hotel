bf548778f93b5cecf08e6000351db2e23aa3b252
jiny <zumod3naranja@gmail.com>
2023-03-28 23:58:16 +0900

    Refactor : RoomCategory import

--- src/main/java/com/sh/vo/RoomCategory.java
+++ src/main/java/com/sh/vo/RoomCategory.java
@@ -1,12 +1,9 @@
 package com.sh.vo;
 
-import java.util.List;
 
 import org.apache.ibatis.type.Alias;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
