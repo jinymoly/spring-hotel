f012c74e88c8a4be115b1c3d17476b484e3205ec
jiny <zumod3naranja@gmail.com>
2023-06-09 23:33:31 +0900

    Feat : errorCode 추가

--- src/main/java/com/sh/exception/ErrorCode.java
+++ src/main/java/com/sh/exception/ErrorCode.java
@@ -10,7 +10,8 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorCode {
 
     NOT_FOUND_ROOM_REV_INFO(HttpStatus.NOT_FOUND, "404", "예약 번호가 존재하지 않습니다."), 
-    NOT_FOUND_USER_INFO(HttpStatus.NOT_FOUND, "404", "예약 이름이 존재하지 않습니다.");
+    NOT_FOUND_USER_INFO(HttpStatus.NOT_FOUND, "404", "예약 이름이 존재하지 않습니다."),
+    NOT_FOUND_ROOM_REV(HttpStatus.NOT_FOUND, "404", "이미 취소된 예약입니다.");
     
     private final HttpStatus status;
     private final String eCode;
