e7fd44ad4707e4695ac1be8d95bf16c170211e79
jiny <zumod3naranja@gmail.com>
2023-06-12 23:47:04 +0900

    Feat : 예약 변경시 사용 dto

--- src/main/java/com/sh/dto/RoomRevUpdateDto.java
+++ src/main/java/com/sh/dto/RoomRevUpdateDto.java
@@ -3,34 +3,29 @@ package com.sh.dto;
 import java.util.Date;
 
 import org.apache.ibatis.type.Alias;
+import org.springframework.lang.Nullable;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AccessLevel;
-import lombok.Builder;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
+@Setter
 @ToString
-@NoArgsConstructor (access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 @Alias("RoomRevUpdateDto")
 public class RoomRevUpdateDto {
     
     private int no; // 예약 번호
     
+    @Nullable
     private String optionCheckinTime; // 체크인 시간
+    @Nullable
     private String request;
 
     @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private Date updatedDate; // 업데이트 날짜 
-
-    @Builder
-    public RoomRevUpdateDto(int no, String optionCheckinTime, String request, Date updatedDate) {
-        this.no = no;
-        this.optionCheckinTime = optionCheckinTime;
-        this.request = request;
-        this.updatedDate = updatedDate;
-    }
-
 }
